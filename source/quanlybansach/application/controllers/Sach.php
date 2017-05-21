9571bee4aaa46028c09d528c42de58cfc712fab7
Trong <lvtrong1993@gmail.com>
2017-05-21 18:55:50 +0700

    modul Quan ly Sach

--- source/quanlybansach/application/controllers/Sach.php
+++ source/quanlybansach/application/controllers/Sach.php
@@ -15,9 +15,53 @@ class Sach extends CI_Controller
 	{
 		$data['data_info'] = $this->Sach_model->get_AllSach();
 		//print_r($data['data_info']);
-	   $data['subview'] ='admin/book/index';
-       $data['title'] = 'Thay đổi quy định';
-       $this->load->view('admin/layout', $data);
+		$data['subview'] ='admin/book/index';
+		$data['title'] = 'Thay đổi quy định';
+		$this->load->view('admin/layout', $data);
+
+	}
+	public function chitiet()
+	{
+		header('Content-Type: application/json');
+		$idSach= $this->input->post('id');
+		$data['data_info']=$this->Sach_model->getSachByID($idSach);
+		//$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+		echo json_encode($data);
+
+		//print_r($data['data_info']);
+
+	}
+
+	public function capnhat()
+	{
+
+		
+
+		$dataSach=array(
+			"masach" => $this->input->post('txtMaSach'),
+			"tensach" => $this->input->post('txtTenSach'),
+			"giaban"=> $this->input->post('txtGiaBan'),
+			"soluong" => $this->input->post('txtSoLuong'),
+			"ngaynhap" => $this->input->post('txtNgayNhap'),
+			"tacgia" => $this->input->post('txtTacGia'),
+			"mancc" => $this->input->post('txtNCC'),
+			"nhaxuatban" => $this->input->post('txtNXB'),
+			"theloai" => $this->input->post('txtTheLoai'),
+			);
+		header('Content-Type: application/json');
+		$effect=$this->Sach_model->updateSach($dataSach['masach'],$dataSach);
+		//echo $effect; 
+		if($effect==1)
+		{
+			$data = array(
+				"status"=>"1",
+				"message"=>"Thành công"
+				);
+			//print_r($data);
+			//echo 1;
+			echo json_encode($data);
+		}
+		
 
 	}
 }
--- source/quanlybansach/application/views/admin/book/index.php
+++ source/quanlybansach/application/views/admin/book/index.php
@@ -32,116 +32,193 @@
 
                 <th>Mã Sách</th>
                 <th>Tên Sách</th>
-                <th>Chi tiết</th>
+
                 <th>Sửa</th>
                 <th>Xóa</th>
               </tr>
 
             </thead>
             <tbody>
-            <?php 
-            $stt =0;
-            foreach ($data_info as $key => $value) {
-                    $stt++;
-                    $masach = $value['masach'];
-            ?>
+              <?php 
+              $stt =0;
+              foreach ($data_info as $key => $value) {
+                $stt++;
+                $masach = $value['masach'];
+                ?>
                 
-                    <tr>
-                     <td>
-                       <div class='text-center'>
-                        <?php echo $stt; ?>
-                       </div>
-                     </td>
+                <tr>
+                 <td>
+                   <div class='text-center'>
+                    <?php echo $stt; ?>
+                  </div>
+                </td>
 
-                     <td> <?php echo $value['masach']; ?></td>
-                     <td><?php $value['tensach']; ?></td>
-                     <td><a class='chitietsach' href='#?masach1=$masach' data-id="<?php echo $masach ?>" data-toggle='modal' data-target='#myModal'>Chi tiết</a></td>
+                <td> <?php echo $value['masach']; ?></td>
+                <td><?php echo $value['tensach']; ?></td>
 
-                     <td><a href='#'>Sửa</a></td>
-                     <td><a href='#'>Xóa</a></td>
-                   </tr>
-               <?php   } ?>
-           
+
+                <td><a class='chitietsach' href='' data-id='<?php echo $masach ;?>' data-toggle='modal' data-target='#myModal'>Sửa</a></td>
+                <td><a href='#'>Xóa</a></td>
+              </tr>
+              <?php   } ?>
+
             </tbody>
           </table>
         </div>
       </div>
-      </div>
     </div>
-    </div>
+  </div>
+</div>
 </div>
 <div class="clear-fix"></div>
- <div class="modal fade col-md-12" id="myModal" role="dialog">
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
-              <h4 class="modal-title">THÔNG TIN SÁCH</h4>
-            </div>
-            <div class="modal-body">
+<div class="modal fade col-md-12" id="myModal" role="dialog">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">THÔNG TIN SÁCH</h4>
+       
+        <div class="alert-success" id="message" ">
+         
+        </div>
+      </div>
+
+      <div class="modal-body">
 
 
 
-              <div class="row"> 
-                <div class="col-md-offset-1 col-md-12"> 
-                  <form action="#" method="post" class="form-horizontal" id="frm-hang-hoa"> 
-                   <!-- <div class="header"><label>Thông tin sách:</label></div> -->
-                   <div class="form-content"> 
+        <div class="row"> 
+          <div class="col-md-offset-1 col-md-12"> 
+            <form action="#" method="post" class="form-horizontal" id="frmSach" enctype="multipart/form-data"> 
+             <!-- <div class="header"><label>Thông tin sách:</label></div> -->
+             <div class="form-content"> 
 
-                    <div class="form-group">
-                      <div class="col-sm-6">
-                        <div class="col-sm-12"><label>Tên sách:</label> <input class="form-control" id="txtTenSach" placeholder="Mã Sách" type="text" value="You are the aple of my eye" readonly>  
-                        </div>
-                        <div class="col-sm-6"><label>Mã sách:</label> <input class="form-control" id="txtMaSach" placeholder="Mã Sách" type="text" value="01" readonly>  
-                        </div> 
-
-                        <div class="col-sm-6"> <label>Ngày nhập:</label><input class="form-control" id="txtNgayNhap" placeholder="Tên sách" type="text" value="1/1/2017" readonly>
-                        </div> 
-                        <div class="col-sm-6"><label>Số lượng:</label> <input class="form-control" id="txtMaSach" placeholder="Số lượng" type="text" value="100" readonly>  
-                        </div> 
-
-                        <div class="col-sm-6"> <label>Giá bán:</label> <input class="form-control" id="txtSoLuong" placeholder="Giá bán" type="text" value="100000" readonly>
-                        </div> 
-                      </div>
-                      <div class="col-sm-3" style="min-height: 180px;">
-                        <br>
-                        <img class="img-thumbnail" src="../../dist/img/you-are-the-aple-of-my-eye.jpg" alt="Chania">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <div class="col-sm-12">
-                        <div class="col-sm-5"> <label>Thể loại:</label><input class="form-control" id="txtTheLoai" placeholder="Thể loại" type="text" value="Truyện tình" readonly> 
-                        </div> 
-                        <div class="col-sm-5"> <label>Tác giả:</label><input class="form-control" id="txtTacGia" placeholder="Tác giả" type="text" value="Cửu Bả Đao" readonly> 
-                        </div> 
-                      </div> 
-                    </div>
-                    <div class="form-group">
-                      <div class="col-sm-12">
-                        <div class="col-sm-5"> <label>Nhà xuất bản:</label><input class="form-control" id="txtNXB" placeholder="Nhà xuất bản" type="text" value="Tuổi trẻ" readonly> 
-                        </div> 
-                        <div class="col-sm-5"> <label>Nhà cung cấp:</label><input class="form-control" id="txtNCC" placeholder="Nhà cung cấp" type="text" value="ATM Education" readonly> 
-                        </div> 
-                      </div> 
-                    </div>
-
-
-
+              <div class="form-group">
+                <div class="col-sm-6">
+                  <div class="col-sm-12"><label>Tên sách:</label> <input class="form-control" name="txtTenSach" id="txtTenSach" placeholder="Tên Sách" type="text" value="" >  
+                  </div>
+                  <div class="col-sm-6"><label>Mã sách:</label> <input class="form-control" name="txtMaSach" id="txtMaSach" placeholder="Mã Sách" type="text" value=""  readonly>  
                   </div> 
+
+                  <!--<div class="col-sm-6"> <label>Ngày nhập:</label><input class="form-control" id="txtNgayNhap" placeholder="Tên sách" type="text" value="" >
+                </div> -->
+                <div class="col-sm-6"> <label>Ngày nhập:</label>
+                  <div class="input-group date">
+                    <div class="input-group-addon">
+                      <i class="fa fa-calendar"></i>
+                    </div>
+                    <input type="text" class="form-control pull-right" name="txtNgayNhap" id="txtNgayNhap"> <!--datepicker -->
+                  </div>
+                </div>
+
+                <div class="col-sm-6"><label>Số lượng:</label> <input class="form-control" name="txtSoLuong" id="txtSoLuong" placeholder="Số lượng" type="text" value="" >  
+                </div> 
+
+                <div class="col-sm-6"> <label>Giá bán:</label> <input class="form-control" name="txtGiaBan" id="txtGiaBan" placeholder="Giá bán" type="text" value="" >
+                </div> 
+              </div>
+              <div class="col-sm-3" style="min-height: 180px;">
+                <br>
+                <img class="img-thumbnail" name="imgHinhSach" id="imgHinhSach" src="" alt="Chania"/>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-12">
+                <div class="col-sm-5"> <label>Thể loại:</label><input class="form-control" name="txtTheLoai" id="txtTheLoai" placeholder="Thể loại" type="text" value="" > 
+                </div> 
+                <div class="col-sm-5"> <label>Tác giả:</label><input class="form-control" name="txtTacGia" id="txtTacGia" placeholder="Tác giả" type="text" value="" > 
+                </div> 
+              </div> 
+            </div>
+            <div class="form-group">
+              <div class="col-sm-12">
+                <div class="col-sm-5"> <label>Nhà xuất bản:</label><input class="form-control" name="txtNXB" id="txtNXB" placeholder="Nhà xuất bản" type="text" value="" > 
+                </div> 
+                <div class="col-sm-5"> <label>Nhà cung cấp:</label><input class="form-control" name="txtNCC" id="txtNCC" placeholder="Nhà cung cấp" type="text" value="" > 
+                </div> 
+              </div> 
+            </div>
+
+
+
+          </div> 
 
                  <!-- <div class="footer clearfix"> 
                     <button type="button" class="btn btn-default">Xác nhận</button> 
                   </div> -->
-                </form> 
+
+                </div> 
               </div> 
-            </div> 
 
 
 
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          </div>
-        </div>
-      </div>
-    </div>
+            </div>
+            <div class="modal-footer">
+             <div class="col-xs-6 col-md-6 text-right">
+               <br><label><button id="btnHuy" type="submit" class="btn btn-default">Hủy</button></label> 
+             </div>
+
+             <div class="col-xs-6 col-md-6 text-left">
+               <br><label><button id="btnCapNhat" type="submit" class="btn btn-primary">Cập nhật</button></label> 
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </form> 
+   <div class="row"></div>
+   <script>
+    $(".chitietsach").click(function(){
+      console.log($(this).data('id'));
+      $.ajax({
+        type: "POST",
+        dataType: 'json',
+        url:"<?php echo site_url('sach/chitiet') ;?>",
+        data : {
+          id:$(this).data('id')
+        },
+        success: function(resp){
+         // alert("vv");
+        // console.log(resp);
+        $('#txtMaSach').val(resp.data_info[0].masach);
+        $('#txtTenSach').val(resp.data_info[0].tensach);
+        $('#txtNgayNhap').val(resp.data_info[0].ngaynhap);
+        $('#txtSoLuong').val(resp.data_info[0].soluong);
+        $('#txtGiaBan').val(resp.data_info[0].giaban);
+        $('#txtTacGia').val(resp.data_info[0].tacgia);
+        $('#txtTheLoai').val(resp.data_info[0].theloai);
+        $('#txtNXB').val(resp.data_info[0].nhaxuatban);
+        $('#txtNCC').val(resp.data_info[0].mancc);
+        var hinh="<?php echo base_url().'public/'; ?>dist/img/"+resp.data_info[0].hinhanh;
+        $("#imgHinhSach").attr("src",hinh);
+      },
+      error: function(resp) { alert(JSON.stringify(resp)); }
+    });
+    });
+
+    $("#btnCapNhat").click(function()
+    {
+      var dataform = $("#frmSach").serialize();
+     // console.log(dataform);
+
+     $.ajax({
+      type: "POST",
+      dataType: 'json',
+      url:"<?php echo site_url('sach/capnhat') ;?>",
+      data : dataform,
+      success: function(resp){
+        console.log(resp.message);
+       //alert(resp.status);
+     //  alert("ccc");
+     $("#message").text(resp.message);
+
+       setTimeout(window.location.reload(), 2000);
+     },
+     error: function(resp) { alert(JSON.stringify(resp)); }
+   });
+     return false;    
+   });
+
+
+ </script>
+
